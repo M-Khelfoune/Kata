2b703dc1ed3d6717ce6434e7c4612b0f68cde256
manvantara <allo3000@msn.com>
2016-09-27 13:22:07 +0200

    First project of calculator

--- app/src/main/java/com/hbdriod/kata/MainActivity.java
+++ app/src/main/java/com/hbdriod/kata/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hbdriod.kata;
 
+import android.graphics.drawable.shapes.Shape;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EditText editText = (EditText) findViewById(R.id.edit_text);
+        editText.setText("Tapez ici votre calcul");
+
+        Button button = (Button) findViewById(R.id.button);
+
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText("Résultat");
+
     }
+
 }
