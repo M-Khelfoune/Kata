90dc6d39fa88f5678f403224d89d9946084c081f
manvantara <allo3000@msn.com>
2016-09-27 17:02:40 +0200

    add unit test calculator

--- app/src/main/java/com/hbdriod/kata/Calculator.java
+++ app/src/main/java/com/hbdriod/kata/Calculator.java
@@ -10,6 +10,18 @@ public class Calculator {
 
     public int add(String numbers) {
         int sum = 0;
+        boolean exist_delimiter = false;
+
+        if (numbers.length()>1){
+            if ((numbers.charAt(0)=='/') && (numbers.charAt(1)=='/')){
+
+
+//                    && (numbers.charAt(1))){
+//                exist_delimeter = true;
+            }
+        }
+
+
         StringTokenizer sl = new StringTokenizer(numbers, "\n");
         while (sl.hasMoreElements()) {
             numbers = sl.nextElement().toString();
--- app/src/test/java/com/hbdriod/kata/ExampleUnitTest.java
+++ app/src/test/java/com/hbdriod/kata/ExampleUnitTest.java
@@ -9,12 +9,33 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void test_string_null() throws Exception {
         Calculator calculator = new Calculator();
-//        assertEquals(0, calculator.add(""));
-//        assertEquals(13, calculator.add("13"));
-//        assertEquals(37, calculator.add("13,24"));
-//        assertEquals(55, calculator.add("8,10,13,24"));
-//        assertEquals(4, calculator.add("1\n1,2"));
+        assertEquals(0, calculator.add(""));
     }
+
+    @Test
+    public void test_string_one_number() throws Exception {
+        Calculator calculator = new Calculator();
+        assertEquals(13, calculator.add("13"));
+    }
+
+    @Test
+    public void test_string_two_number() throws Exception {
+        Calculator calculator = new Calculator();
+        assertEquals(37, calculator.add("13,24"));
+    }
+
+    @Test
+    public void test_string_many_number() throws Exception {
+        Calculator calculator = new Calculator();
+        assertEquals(55, calculator.add("8,10,13,24"));
+    }
+
+    @Test
+    public void test_many_line_many_number() throws Exception {
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.add("1\n1,2"));
+    }
+
 }
